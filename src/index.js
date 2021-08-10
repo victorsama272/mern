3c811d1157f4a1b4964bfcc3b050d59e3cfa445d
victorsama272 <victormoreno272@gmail.com>
2021-08-10 16:25:50 -0500

    tarea

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require ('express');
+const morgan = require ('morgan');
+const path = require('path');
+
+const app = express();
+
+//settings
+app.set('port', process.env.PORT || 3000 );
+
+
+//Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+
+//Routes
+app.use('/api/tasks', require('./routes/task.routers'));
+//static files
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// starting the server
+
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+});
